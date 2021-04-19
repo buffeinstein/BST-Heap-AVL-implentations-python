b439123373fa3bab40f9a165a5e9f6639e399024
Mike Izbicki <mike@izbicki.me>
2021-04-18 21:39:03 -0700

    heap functions

---
+++ containers/Heap.py
@@ -0,0 +1,103 @@
+'''
+This file implements the Heap data structure as a subclass of the BinaryTree.
+The book implements Heaps using an *implicit* tree with an *explicit* vector implementation,
+so the code in the book is likely to be less helpful than the code for the other data structures.
+The book's implementation is the traditional implementation because it has a faster constant factor
+(but the same asymptotics).
+This homework is using an explicit tree implementation to help you get more practice with OOP-style programming and classes.
+'''
+
+from containers.BinaryTree import BinaryTree, Node
+
+
+class Heap():
+    '''
+    FIXME:
+    Heap is currently not a subclass of BinaryTree.
+    You should make the necessary changes in the class declaration line above
+    and in the constructor below.
+    '''
+
+    def __init__(self, xs=None):
+        '''
+        FIXME:
+        If xs is a list (i.e. xs is not None),
+        then each element of xs needs to be inserted into the Heap.
+        '''
+
+    def __repr__(self):
+        '''
+        Notice that in the BinaryTree class,
+        we defined a __str__ function,
+        but not a __repr__ function.
+        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
+        Thus, if you create a variable using the command Heap([1,2,3])
+        it's __repr__ will return "Heap([1,2,3])"
+
+        For the Heap, type(self).__name__ will be the string "Heap",
+        but for the AVLTree, this expression will be "AVLTree".
+        Using this expression ensures that all subclasses of Heap will have a correct implementation of __repr__,
+        and that they won't have to reimplement it.
+        '''
+        return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
+
+    def is_heap_satisfied(self):
+        '''
+        Whenever you implement a data structure,
+        the first thing to do is to implement a function that checks whether
+        the structure obeys all of its laws.
+        This makes it possible to automatically test whether insert/delete functions
+        are actually working.
+        '''
+        if self.root:
+            return Heap._is_heap_satisfied(self.root)
+        return True
+
+    @staticmethod
+    def _is_heap_satisfied(node):
+        '''
+        FIXME:
+        Implement this method.
+        '''
+
+    def insert(self, value):
+        '''
+        Inserts value into the heap.
+
+        FIXME:
+        Implement this function.
+
+        HINT:
+        Create a @staticmethod helper function,
+        following the same pattern used in the BST and AVLTree insert functions.
+        '''
+
+    def insert_list(self, xs):
+        '''
+        Given a list xs, insert each element of xs into self.
+
+        FIXME:
+        Implement this function.
+        '''
+
+    def find_smallest(self):
+        '''
+        Returns the smallest value in the tree.
+
+        FIXME:
+        Implement this function.
+        '''
+
+    def remove_min(self):
+        '''
+        Removes the minimum value from the Heap.
+        If the heap is empty, it does nothing.
+
+        FIXME:
+        Implement this function.
+
+        HINT:
+        I created two @staticmethod helper functions: _trickle and _remove_bottom_right.
+        It's possible to do it with only a single helper (or no helper at all),
+        but I personally found dividing up the code into two made the most sense.
+        '''
