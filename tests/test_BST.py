a71100e1b5ca19b5f68870d9bf9d650da6402eb0
Mike Izbicki <mike@izbicki.me>
2023-03-25 21:28:12 -0700

    fix test case

--- tests/test_BST.py
+++ tests/test_BST.py
@@ -283,7 +283,6 @@ def test__BST_inorder_property(xs):
     assert bst1.to_list('inorder') == bst2.to_list('inorder')
 
 
-<<<<<<< HEAD
 def test__BST_iterable_1():
     '''
     The BST should be iterable.
@@ -323,10 +322,21 @@ def test__BST_iterable_2(xs):
 
     assert list(bst1) == list(bst2)
 
+
 @given(xs=ints)
 def test__BST_eq(xs):
     '''
     This test is essentially the same as the previous one,
     but tests the == operator specifically.
     '''
+    xs = list(set(xs))
+
+    xs1 = copy.copy(xs)
+    random.shuffle(xs1)
+    bst1 = BST(xs1)
+
+    xs2 = copy.copy(xs)
+    random.shuffle(xs2)
+    bst2 = BST(xs2)
+    
     assert bst1 == bst2
