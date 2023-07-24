d95d0843ea7c444a45f83d04d6aa4f00de0b69d6
buffeinstein <ambikatiwari999@gmail.com>
2023-07-23 23:29:43 -0700

    rebased

--- tests/test_BST.py
+++ tests/test_BST.py
@@ -283,12 +283,41 @@ def test__BST_inorder_property(xs):
     assert bst1.to_list('inorder') == bst2.to_list('inorder')
 
 
+<<<<<<< HEAD
 @given(xs=ints)
 def test__BST_eq(xs):
     '''
     This test is essentially the same as the previous one,
     but tests the == operator specifically.
     '''
+=======
+def test__BST_iterable_1():
+    '''
+    The BST should be iterable.
+    Iterating over the tree should give the results in sorted order.
+
+    HINT:
+    Currently, the BST class is not iterable because it does not define the __iter__ method.
+    Therefore, these tests all fail.
+    You must define an appropriate __iter__ method in order to make these tests pass.
+    I encourage you to think carefully about how you can reuse your previously implemented functions to implement the iterable interface.
+    '''
+    xs = [1, 2, 3, 4, 5]
+    bst = BST(xs)
+    _iter = iter(bst)
+    assert next(_iter) == 1
+    assert next(_iter) == 2
+    assert next(_iter) == 3
+    assert next(_iter) == 4
+    assert next(_iter) == 5
+    try:
+        next(_iter)
+    except StopIteration:
+        pass
+
+
+@given(xs=ints)
+def test__BST_iterable_2(xs):
     xs = list(set(xs))
 
     xs1 = copy.copy(xs)
@@ -300,3 +329,4 @@ def test__BST_eq(xs):
     bst2 = BST(xs2)
 
     assert bst1 == bst2
+    assert list(bst1) == list(bst2)
